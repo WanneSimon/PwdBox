936351c55eada12164e61e00f779261cda2f0aa0
wanne <wanneseville@foxmail.com>
2023-04-10 17:02:37 +0800

    屏幕抓取

--- go.mod
+++ go.mod
@@ -11,9 +11,12 @@ require (
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
+	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // indirect
 	github.com/labstack/echo/v4 v4.9.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/leaanthony/go-ansi-parser v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,114 +1,57 @@
 package main
 
 import (
-	"context"
-	"embed"
 	"fmt"
-	"log"
+	"image"
+	"image/png"
 	"os"
-	"path"
 	"path/filepath"
-	"runtime"
-	"strings"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"github.com/wailsapp/wails/v2/pkg/options/windows"
-	"github.com/wanneSimon/saya-app/conf"
-	"github.com/wanneSimon/saya-app/env"
+	"github.com/kbinani/screenshot"
 )
 
-//go:embed all:frontend/dist
-var assets embed.FS
-
 func main() {
-	// Create an instance of the app structure
-	app := NewApp()
-	fileOp := &env.FileOp{}
+	num := screenshot.NumActiveDisplays()
+	if num <= 0 {
+		panic("Active display not found")
+	}
 
-	// configs
-	var rootPath string = GetCurrentAbPath()
-	// var rootPath string = "D:\\Git_Repo\\saya"
-	// appConfig := strings.Join(rootPath, filepath.Separator, "config", filepath.Separator, "saya.yml")
-	sp := fmt.Sprintf("%c", filepath.Separator)
-	var appConfigPath = rootPath + sp + "config" + sp + "saya.yml"
-	configOps := conf.NewConfigOpsAndLoad(appConfigPath)
+	var all image.Rectangle = image.Rect(0, 0, 0, 0)
 
-	appConfig := configOps.Get()
+	for i := 0; i < num; i++ {
+		//device, _ := screenshot.CaptureDisplay(i)
+		bounds := screenshot.GetDisplayBounds(i)
+		all = all.Union(bounds)
 
-	fmt.Println("appconfig", appConfig)
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf("test/%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+		save(img, fileName)
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
 
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:     appConfig.Title, //"saya-app",
-		Width:     1024,
-		Height:    768,
-		MinWidth:  930,
-		MinHeight: 490,
-		Frameless: appConfig.Frameless,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		// BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 255},
-		OnStartup: func(ctx context.Context) {
-			app.startup(ctx)
-			app.transparentWinOS(appConfig.Title)
-			fileOp.SetContext(ctx)
-		},
-		Bind: []interface{}{
-			app, fileOp, configOps,
-		},
-		Windows: &windows.Options{
-			WebviewIsTransparent: true,
-			// WindowIsTranslucent:               true,
-			DisableWindowIcon:                 false,
-			DisableFramelessWindowDecorations: false,
-			WebviewUserDataPath:               "userdata",
-			Theme:                             windows.SystemDefault,
-			CustomTheme: &windows.ThemeSettings{
-				DarkModeTitleBar:   windows.RGB(20, 20, 20),
-				DarkModeTitleText:  windows.RGB(200, 200, 200),
-				DarkModeBorder:     windows.RGB(20, 0, 20),
-				LightModeTitleBar:  windows.RGB(200, 200, 200),
-				LightModeTitleText: windows.RGB(20, 20, 20),
-				LightModeBorder:    windows.RGB(200, 200, 200),
-			},
-		},
-	})
-
+	// Capture all desktop region into an image.
+	fmt.Printf("%v\n", all)
+	img, err := screenshot.Capture(all.Min.X, all.Min.Y, all.Dx(), all.Dy())
 	if err != nil {
-		println("Error:", err.Error())
+		panic(err)
 	}
+	save(img, "test/all.png")
 }
 
-// 获取当前路径
-func GetCurrentAbPath() string {
-	dir := getCurrentAbPathByExecutable()
-	tmpDir, _ := filepath.EvalSymlinks(os.TempDir())
-	if strings.Contains(dir, tmpDir) {
-		return getCurrentAbPathByCaller()
+func save(img *image.RGBA, fileName string) {
+	parent := filepath.Dir(fileName)
+	if parent != "" {
+		os.MkdirAll(parent, 0666)
 	}
-	return dir
-}
 
-// 获取当前执行文件绝对路径
-func getCurrentAbPathByExecutable() string {
-	exePath, err := os.Executable()
+	f, err := os.Create(fileName)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	res, _ := filepath.EvalSymlinks(filepath.Dir(exePath))
-	return res
-}
+	defer f.Close()
 
-// 获取当前执行文件绝对路径（go run）
-func getCurrentAbPathByCaller() string {
-	var abPath string
-	_, filename, _, ok := runtime.Caller(0)
-	if ok {
-		abPath = path.Dir(filename)
-	}
-	return abPath
+	png.Encode(f, img)
 }
